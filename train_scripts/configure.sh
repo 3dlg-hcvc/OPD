b776a97c1539bc4b1eee1f1a6c0f245ae820793e
Hanxiao Jiang <shawn.x.jiang@gmail.com>
2022-03-18 10:07:58 -0700

    clean the scripts

--- train_scripts/configure.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-export MNET_VENV=/home/hanxiao/scratch/proj-motionnet/run_env
-export PROJ_DIR=/home/hanxiao/projects/rrg-msavva/hanxiao/proj-motionnet/2DMotion
-export DATASET_DIR=/home/hanxiao/projects/rrg-msavva/hanxiao/proj-motionnet/Dataset
-# The below two stuff need to be set in the generation script
-export EMAIL_ADDR=shawn_jiang@sfu.ca
-export OUTPUT_DIR=/home/hanxiao/scratch/proj-motionnet/train_output
